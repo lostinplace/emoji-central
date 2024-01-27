e3932dbb61b5731575c5c2dac68d6217ff4a014b
Chris Wheeler <cmwhee@gmail.com>
2024-01-27 14:45:31 -0500

    compiles but doesn't work

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -2,16 +2,31 @@ extends Node
 
 var Player1
 var UINode
+@onready var Player = preload("res://Scenes/player.tscn")
+@onready var singleton = get_node("/root/Singleton")
+
+var players: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Player1 = get_node("Player1")
-	var Player1_rects = Player1.queue_rects
-	$UI.update_queue_sprites(1, Player1_rects)
+	
+	for i in singleton.playerCount:
+		var player = Player.instantiate()
+		player.playerNum = i
+		player.global_position = Vector2(randi_range(500, 1000), randi_range(100,700))
+		add_child(player)
+		players.append(player)
+		
+	
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var Player1_rects = Player1.queue_rects
-	$UI.update_queue_sprites(1, Player1_rects)
+	for i in players.size():
+		var this_player = players[i]
+		var player_rects = this_player.queue_rects
+		$UI.update_queue_sprites(i, player_rects)
 	
+	
+
