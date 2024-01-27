a9d7fe9b01cbf12a128a059fcec520aff9563610
ortiz-da <ortiz.da@northeastern.edu>
2024-01-27 16:37:23 -0500

    321 countdown

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -5,23 +5,20 @@ extends Node
 @onready var cloudTimer = $Timer
 @onready var plCloud = preload("res://Scenes/cloud.tscn")
 
-var playersAlive
+
 var players: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	playersAlive = singleton.playerCount
+	
 	for i in singleton.playerCount:
 		var player = Player.instantiate()
 		player.playerNum = i
-		player.main = self
 		player.global_position = Vector2(randi_range(500, 1000), randi_range(100,700))
 		add_child(player)
 		players.append(player)
 		
 
-func player_dies(player):
-	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	for i in players.size():
@@ -31,7 +28,7 @@ func _process(delta):
 	
 	
 func _on_timer_timeout():
-	cloudTimer.wait_time = randf_range(15,25)
+	cloudTimer.wait_time = randf_range(5,15)
 	var cloud = plCloud.instantiate()
 	add_child(cloud)
 	cloud.global_position = Vector2 (0, randi_range(0,650))
