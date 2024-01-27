910d1b7ef33945c3d34879a2bd9a0f9636da737e
LeftyAtHeart <62956483+LeftyAtHeart@users.noreply.github.com>
2024-01-27 10:28:29 -0500

    Revert "fixed conflicts player sprite stuff"
    This reverts commit e87e03d2991249eaca7e0e47693fb9f05534b926.

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 var velocity
-var speed = 5
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,4 +12,3 @@ func _process(delta):
 
 func _physics_process(delta):
 	global_position += velocity * delta
-	global_position += velocity * speed
