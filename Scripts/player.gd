5fb7717f91e882032943c0c3683d68971f9ebe9e
Chris Wheeler <cmwhee@gmail.com>
2024-01-28 14:50:20 -0500

    fixed bug with outlines

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -192,7 +192,7 @@ func _on_area_2d_body_entered(body):
 		var impacting_joke_type = body.joke.Category
 		var my_category = my_joke_hopper.my_category["category"]
 		if my_category == impacting_joke_type:
-			damage(10 * damageMod);
+			damage(20 * damageMod);
 		else:
 			$SplatAudio.play()
 		body.queue_free();
